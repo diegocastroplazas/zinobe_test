51e2fcc3e174b2b3d30664be22bed627c6409402
Diego Castro <diegomecatronico@gmail.com>
2020-02-25 18:43:45 -0500

    Version funcional

---
+++ geoetl.py
@@ -0,0 +1,44 @@
+import pandas as pd
+import numpy as np
+import requests
+import json
+import hashlib
+import timeit
+
+class GeoEtl(object):
+    def __init__(self):
+        pass
+
+    def _getRegions(self):
+        URL = "https://restcountries-v1.p.rapidapi.com/all"
+        HEADERS = {
+            'x-rapidapi-host': "restcountries-v1.p.rapidapi.com",
+            'x-rapidapi-key': "729324cea1mshc6255d3b0979a04p1d233ejsnbb643168d529"
+        }
+        response = requests.get(URL, headers=HEADERS)
+        response_dict = json.loads(response.text)
+        regions_df = pd.DataFrame(response_dict).replace(r'^\s*$', np.nan, regex=True)
+        regions = regions_df['region'].unique()
+        return regions
+
+
+    def _getCountryByRegion(self, region=''):
+        URL = "https://restcountries.eu/rest/v2/region/{0}".format(region.lower())
+        r = requests.get(URL)
+        country = json.loads(r.text)[0]
+        language = country['languages'][0]['name']
+        encrypted_language = hashlib.new("sha1", language.encode())
+        required_data = {
+            'region': region,
+            'country': country['name'],
+            'language': str(encrypted_language.hexdigest())
+        }
+        return required_data
+
+    def obtainData(self):
+        regions = self._getRegions()
+        for region in regions:
+            startime = timeit.default_timer()
+            country_data = self._getCountryByRegion(region)
+            country_data['time'] = timeit.default_timer() - startime
+            print(country_data)
---
+++ main.py
@@ -0,0 +1,5 @@
+from geoetl import GeoEtl
+
+if __name__ == "__main__":
+    getl = GeoEtl()
+    getl.obtainData()
